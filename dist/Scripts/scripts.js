63d99da3d67b190a234dd1eb6763443557e6357f
ZarRza <160187613+ZarRza@users.noreply.github.com>
2024-04-11 06:45:41 +0430

    Add more toolbarOptions

--- dist/Scripts/scripts.js
+++ dist/Scripts/scripts.js
@@ -1,6 +1,29 @@
+const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        
+    ['blockquote', 'code-block'],
+    ['link', 'formula'],
+  
+    [{ 'header': 1 }, { 'header': 2 }],               
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      
+    [{ 'indent': '-1'}, { 'indent': '+1' }],           
+    [{ 'direction': 'rtl' }],                          
+  
+    [{ 'size': ['small', false, 'large', 'huge'] }],   
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+    [{ 'color': [] }, { 'background': [] }],           
+    [{ 'align': [] }],
+  
+    ['clean']                                         
+  ];
+
 const quill = new Quill('#editor', {
+    modules: {
+      toolbar: toolbarOptions
+    },
     theme: 'snow'
-});
+  });
 
 document.getElementById('save').addEventListener('click', function () {
     var editorContent = quill.root.innerHTML;
